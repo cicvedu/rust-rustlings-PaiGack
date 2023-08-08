dff7cefd6a74b95b67a50c7e1148a1c028935274
PaiGack <460852283@qq.com>
2023-08-08 15:00:52 +0800

    fix: drive5

--- exercises/drive/drive5.rs
+++ exercises/drive/drive5.rs
@@ -4,14 +4,15 @@
 // code in the testcase should call the same function.
 // You should not modify any existing code. All you need to do is add two line of attributes.
 
-// I AM NOT DONE
-
 extern "C" {
+    #[link_name = "my_demo_function"]
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
+    #[no_mangle]
     fn my_demo_function(a: u32) -> u32 {
         a
     }
